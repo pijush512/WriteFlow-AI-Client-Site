916bd6944a4b34d22635655b1e3753f4dbde3007
pijush512 <psagorsarker512@gmail.com>
2026-05-29 18:47:55 +0600

    Fix next config and prepare for deploy

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,7 @@ const nextConfig: NextConfig = {
     // বিল্ডের সময় TypeScript এররগুলোকে ইগনোর করবে
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // বিল্ডের সময় ESLint এররগুলোকেও ইগনোর করবে
-    ignoreDuringBuilds: true,
-  },
+
 };
 
 export default nextConfig;

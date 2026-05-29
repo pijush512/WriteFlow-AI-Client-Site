1edc37878605f68af350e2bfc888d5da26e5e995
pijush512 <psagorsarker512@gmail.com>
2026-05-29 18:33:57 +0600

    Update next config to ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // বিল্ডের সময় TypeScript এররগুলোকে ইগনোর করবে
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // বিল্ডের সময় ESLint এররগুলোকেও ইগনোর করবে
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
